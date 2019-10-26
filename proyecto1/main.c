40fe69ba194879527ffb07bf024b4ea264638d1f
cccsar <15-11295@usb.ve>
2019-10-25 21:56:37 -0400

    mas aun

--- proyecto1/main.c
+++ proyecto1/main.c
@@ -27,10 +27,10 @@
 int main(int argc, char** argv) {
 	
 	/* CREACION */
-	//createMyTar(argc, argv);
+	createMyTar(argc, argv);
 
 	/* EXTRACCION */
-	extractMyTar(argv);
+	//extractMyTar(argv);
 
 	return 0;
 }
