c0422c10e8a6ef11108b92213b9c4796c7dfe0b2
S1V1R0 <carlos.csivira@gmail.com>
2019-12-04 23:41:01 -0400

    Ajustes en comentarios

--- proyecto2/siv_testing/singly.h
+++ proyecto2/siv_testing/singly.h
@@ -1,11 +1,24 @@
+/*
+ * Archivo: singly.h 
+ *
+ * Descripcion:	Archivo de cabecera para la estructura singly.
+ *
+ * Autores:
+ * 	Carlos Alejandro Sivira Munoz		15-11377
+ * 	Cesar Alfonso Rosario Escobar		15-11295
+ *
+ */
 
 #ifndef __SINGLY__
 #define __SINGLY__
 
+
 /* snode
  * --------------
+ * Inicializa nodo de lista singly
  *
- *
+ * 	n_: apuntador al nodo a inicializar
+ *	number: inodo que acompana al nodo
  */
 typedef struct singly_node {
 	struct singly_node *next; 
@@ -15,8 +28,9 @@ typedef struct singly_node {
 
 /* singly 
  * --------------
+ * Estructura singly
  *
- *
+ * 	lista simplemente enlazada	
  */
 typedef struct {
 	snode *head, *tail; 
@@ -26,23 +40,28 @@ typedef struct {
 
 /* snodeInit
  * --------------
+ * Inicializa nodo de lista singly
  *
- *
- */
+ * 	n_: apuntador al nodo a inicializar
+ *	number: inodo que acompana al nodo
+*/
 void snodeInit(snode *n_, int number); 
 
 
 /* singlyInit
  * --------------
+ * Inicializa una lista singly
  *
- *
+ * 	l_: apuntador a una lista
  */
 void singlyInit(singly *l_) ;
 
 
 /* singlyDestroy
  * --------------
+ * Libera la memoria de una lista singly
  *
+ * 	l_: lista a destruir
  *
  */
 void singlyDestroy(singly *l_) ;
@@ -50,7 +69,10 @@ void singlyDestroy(singly *l_) ;
 
 /* singlyInsert
  * --------------
+ * Inserta un nodo en una lista singly
  *
+ * 	l_: apuntador a la lista en donde se insertara
+ * 	n_: apuntador del nodo a insertar
  *
  */
 void singlyInsert(singly *l_, snode *n_) ;
@@ -58,25 +80,12 @@ void singlyInsert(singly *l_, snode *n_) ;
 
 /* singlySearch
  * --------------
+ * Revisa si un existe un nodo en la lista con un inodo asociado dado.
  *
+ * 	l_: apuntador a la lista
+ * 	number: inodo a consultar
  *
  */
 int singlySearch(singly *l_, int number) ;
-
-
-/* singlyPrint
- * --------------
- *
- *
- */
-void singlyPrint(singly *l_) ; 
-
-
-/* singlyTesting
- * --------------
- *
- *
- */
-void singlyTesting() ;
 
 #endif
